8087dfd4ddc106c829c0ef362c13369976b89fa6
barsie <barsiebarila@gmail.com>
2025-05-07 23:48:14 +0100

    Added code that sove the stockPrices.java assignment
    Variable name refinement is needed

--- Coding in Java/Asssignments/StockPrices.java
+++ Coding in Java/Asssignments/StockPrices.java
@@ -1,44 +1,75 @@
 package Asssignments;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class StockPrices {
 
-    static float MAX_STOCK = 10;
 
-    static Object[] stockPrices = {12, 45, 45, 6890, 334};
-
+    
     public static void main(String[] args) {
-        calculateAveragePrice();
-        System.out.println(Arrays.deepToString(stockPrices));
-        System.out.println("A stock price you case");
+        int[] stockGeneralPrices = {12, 45, 45, 6890, 334};
+        ArrayList <Integer> stockList = new ArrayList<>();
+        for (int price : stockGeneralPrices) {
+            stockList.add(price);
+        }
+        
+        // calling methods: 
+        double average = calculateAveragePrice(stockGeneralPrices);
+        System.out.println("We have an average price of: " + average);
+
+        int maximum = findMaximumPrice(stockGeneralPrices);
+        System.out.println("The maximun amount found in the array is: " + maximum);
+
+        int occurance = 50;
+        int count = countOccurrences(stockGeneralPrices, occurance);
+        System.out.println("Repeated occurance " + occurance + ": " + count);
+
+        ArrayList<Integer> acumulado = computeCumulativeSum(stockList);
+        System.out.println("Our stock cumulative is: " + acumulado);
     }
 
     // Calculate the average of stock prices
-    static float calculateAveragePrice(){
-        for(int i = 0; i < stockPrices.length; i++){
-            // float average = stockPrices[i] / i;
-            System.out.println("Stock Price is: "+ stockPrices[i]);
+    static double calculateAveragePrice(int[] stockPrices){
+        int sum = 0; // initiatize incremental
+        for(int price: stockPrices){
+            sum += price;
         }
-        return 0;
+        return (double) sum / stockPrices.length;
 
     }
 
     //Find the maximum stock price
-    
-    static float findMaximumPrice(){
-        return  0;
+    public static int findMaximumPrice(int[] stockPrices){
+        int maxPrice = stockPrices[0];
+        for (int price: stockPrices) {
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+        }
+        return maxPrice;
     }
 
     // Determine the occurances count of a specific price
-    static int countOccurrences(){
+    static int countOccurrences( int[] stockPrices, int occurance){
+        int count = 0; // initialize counter
+        for (int price : stockPrices) {
+            if (price == occurance) { //if count is iqual to occurance
+                count ++; // increment counter
+            }
+        }
         return 0;
     }
 
     // Compute the cumulative sum of stock price:
 
-    static float computeCumulativeSum(){
-        return 0;
+    static ArrayList<Integer> computeCumulativeSum(ArrayList<Integer> stockPrices){
+        ArrayList<Integer> cumulativeSum = new ArrayList<>(); // initialized array list
+        int sum = 0;
+        for (int price : stockPrices) { //for each price in stockPrices
+            sum += price; // increment the sum of stock price 
+            cumulativeSum.add(sum); // add the cumulativeSum value to the Array. 
+        }
+        return cumulativeSum;
     }
 }
 
