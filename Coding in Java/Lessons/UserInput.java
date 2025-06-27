54999aea19d2740468928b51ab7d62b0ed4c4a03
barsie <barsiebarila@gmail.com>
2025-06-26 22:32:23 -0500

    added a new file on Assingment Folder -> Coding with Java

--- Coding in Java/Lessons/UserInput.java
+++ Coding in Java/Lessons/UserInput.java
@@ -9,16 +9,14 @@ public class UserInput {
         // the scanner method work with datatypes and have to be specified
         System.out.print("Age: ");
         byte age = scanner.nextByte();
-        System.out.println("You are: " + age + " years old");
-
-        System.out.print("name: ");
+        
+        // System.out.print("name: ");
         /*this next method only count one token: that is a character or series of characters*/
-        String name = scanner.next();
-
         System.out.print("Full Name: ");
-        String fullName = scanner.nextLine();
-        System.out.println("Your name is: " + name);
-        System.out.println(fullName);
+        String fullname = scanner.next();
+
+        System.out.println("You are: " + age + " years old");
+        System.out.println("Your name is: " + fullname);
         
     }
 }
